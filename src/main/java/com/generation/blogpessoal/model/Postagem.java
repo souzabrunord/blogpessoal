24a938a58e0cc43351ad66da5624c1efc91152fa
souzabrunord <souzabrunord@gmail.com>
2022-02-17 23:11:58 -0300

    blog

---
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -0,0 +1,73 @@
+package com.generation.blogpessoal.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name = "tb_postagens" )
+public class Postagem {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo título é obrigatório!")
+	@Size(min = 5, max = 100, message = "O atributo título deve conter entre 5 e 100 caracteres")
+	private String título;
+	
+	@NotBlank(message = "O atributo título é obrigatório!")
+	@Size(min = 10, max = 1000, message = "O atributo título deve conter entre 10 e 1000 caracteres")
+	private String texto;
+	
+	@UpdateTimestamp
+	private LocalDateTime data;
+	
+	
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTítulo() {
+		return título;
+	}
+
+	public void setTítulo(String título) {
+		this.título = título;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public LocalDateTime getData() {
+		return data;
+	}
+
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+	
+	
+	
+	
+
+}
